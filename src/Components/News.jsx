440570bcfb54b1d977fec17270aff63300e439d6
Your Name <you@example.com>
2022-01-23 12:58:59 +0100

    i began adding news cards tto the news page

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
+import moment from 'moment'
+import { useGetCryptosNewsQuery } from '../services/cryptoNewsApi'
+const demoImage =
+  'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News'
+const { Text, Title } = Typography
+const { Option } = Select
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptosNewsQuery({
+    newsCategory: 'Cryptocurrency',
+    count: simplified ? 6 : 12,
+  })
+  if (!cryptoNews?.value) return 'loading...'
 
-const News = () => {
-  return <div>dggnb</div>
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img src={news?.image?.thumbnail?.contentUrl || demoImage} />
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  )
 }
 
 export default News

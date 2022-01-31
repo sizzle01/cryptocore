efd47282bdbdf4896137593d10fa18a14db3c8e0
Your Name <you@example.com>
2022-01-31 09:55:30 +0100

    changes top crypto text on homepage

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -54,7 +54,7 @@ const HomePage = () => {
       </Row>
       <div className="home-heading-container">
         <Title level={2} className="home-title">
-          Top 10 Cryptocurrencies in the world
+          World top Cryptocurrencies
         </Title>
         <Title level={3} className="show-more">
           <Link to="/cryptocurrencies">Show more</Link>
